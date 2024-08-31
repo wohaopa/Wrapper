5aa0a2b4e0d31f3511d3c641eff3080a2234807a
初夏同学 <2411829240@qq.com>
2024-08-31 09:33:51 +0800

    processor

---
+++ processor.py
@@ -0,0 +1,80 @@
+import json
+
+gtnh_assets = None
+nameMap = {"Buildcraft Oil Tweaks": "BuildCraftOilTweak", "Gravitation Suite": "GraviSuite", "Gravitation-Suite-old": "GraviSuite", "Thaumic Machina": "Thaumic-Machina", "UniLib": "UniLib"}
+
+
+with open("gtnh-assets.json", encoding="utf8", mode="r") as file:
+    gtnh_assets = json.load(file)
+
+
+mods = gtnh_assets["mods"]
+Mods = {}
+for mod in mods:
+    item = {}
+    item["latest"] = mod["latest_version"]
+    if "private" in mod and mod["private"]:
+        item["private"] = True
+
+    vers = {}
+    for version in mod["versions"]:
+        ver = {}
+        ver["name"] = version["filename"]
+        if version["download_url"].startswith("https://api.github.com/repos/"):
+            ver["url"] = version["browser_download_url"]
+        else:
+            ver["url"] = version["download_url"]
+
+        vers[version["version_tag"]] = ver
+
+    item["versions"] = vers
+    if "repo_url" in mod:
+        item["url"] = mod["repo_url"]
+    elif "external_url" in mod:
+        item["url"] = mod["external_url"]
+    else:
+        item["url"] = "<unknown>"
+
+    if item["url"].startswith("https://github.com/GTNewHorizons/"):
+        name = mod["name"]
+        item["id"] = f"com.github.GTNewHorizons:{name}:<version>"
+        item["src"] = "GTNH"
+    elif item["url"].startswith("https://github.com/"):
+        group = item["url"].removeprefix("https://github.com/").split("/", 3)
+        item["id"] = f"com.github.{group[0]}:{group[1]}:<version>"
+        item["src"] = "github"
+    else:
+        name = "<unknown>"
+        if item["url"].startswith("https://www.curseforge.com/minecraft/mc-mods/"):
+            name = item["url"].removeprefix("https://www.curseforge.com/minecraft/mc-mods/").split("/", 2)[0]
+            item["id"] = f"curseforge:{name}:<version>"
+            item["src"] = "curse"
+        elif mod["name"] in nameMap:
+            name = nameMap[mod["name"]]
+            item["id"] = f"other:{name}:<version>"
+            item["src"] = "other"
+    Mods[mod["name"]] = item
+
+Config = {}
+gtnh_config = gtnh_assets["config"]
+config = {}
+config["latest"] = gtnh_config["latest_version"]
+config["url"] = gtnh_config["repo_url"]
+config["versions"] = {}
+for version in gtnh_config["versions"]:
+    ver = {}
+    ver["name"] = version["filename"]
+    ver["url"] = version["filename"]
+    config["versions"][version["version_tag"]] = ver
+
+Config[gtnh_config["name"]] = config
+
+result = {}
+result["mods"] = Mods
+result["config"] = Config
+
+with open("gtnh-assets-wrapper-formatted.json", encoding="utf8", mode="w") as file:
+    json.dump(result, file, sort_keys=True, indent=4)
+
+with open("gtnh-assets-wrapper.json", encoding="utf8", mode="w") as file:
+    json.dump(result, file, sort_keys=True)
